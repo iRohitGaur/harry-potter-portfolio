8800bc40dce6c3803c96a5937ce60eaabe814419
Rohit Gaur <rohitgaur7@gmail.com>
2024-07-25 18:20:07 -0400

    feat: CRUD projects

--- server.js
+++ server.js
@@ -1,14 +1,24 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
 const path = require("path");
+const methodOverride = require("method-override");
 const { sendTelegramNotification } = require("./utils/telegram");
+const projectsRouter = require("./routes/projects");
 const port = 3000;
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.log(err));
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
 
 app.get("/", (req, res) => {
   res.render("pages/home", { page: "home" });
@@ -18,9 +28,7 @@ app.get("/about", (req, res) => {
   res.render("pages/about", { page: "about" });
 });
 
-app.get("/projects", (req, res) => {
-  res.render("pages/projects", { page: "projects" });
-});
+app.use("/projects", projectsRouter);
 
 app.get("/contact", (req, res) => {
   res.render("pages/contact", { page: "contact" });
